c57c53c201452147b4e386ccaba57b2d832d2467
Karthigha J <32119141+kjeyasot@users.noreply.github.com>
2018-10-14 17:02:33 -0400

    added old files

--- src/App.js
+++ src/App.js
@@ -1,28 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Popup from "reactjs-popup";
+
+import * as script from './scripts';
+import * as Modal from './components/modal';
+
+const images = script.importAll(require.context('./images', false, /\.(png|jpe?g|svg)$/));
 
 class App extends Component {
+ 
   render() {
     return (
-      <div className="App">
+           <div className="App">
+       
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <div className="container" >
+                <img className="slide" alt="bridal" src={images['bridal.jpg']}/>
+                <img className="slide" alt="henna" src={images['henna.jpg']} />
+                <img className="slide" alt="hair" src={images['hair.jpg']} />
+                <div>
+    <Popup trigger={<button className="signInBtn"> Sign In </button>} modal>
+    {close => (
+      <div className="modal">
+        <a className="close" onClick={close}>
+          &times;
+        </a> 
+        <div className="content">
+        <Modal.signInModal/> 
+        </div>    
+      </div>  
+    )}
+    </Popup>
+    <Popup trigger={<button className="signUpBtn"> Sign Up </button>} modal >
+        {close => (
+      <div className="modal">
+        <a className="close" onClick={close}>
+          &times;
+        </a> 
+        <div className="content">
+        <Modal.signUpModal/>
+         
+        </div>    
+      </div>  
+        )}
+    </Popup>
+    </div>
+        </div>
         </header>
       </div>
+
+ 
     );
   }
+
+
 }
 
 export default App;
