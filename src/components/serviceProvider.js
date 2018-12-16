5afafd3c0d1181d21b9b6a785c4a02e8918dc11e
ermsci342 <32082256+ermsci342@users.noreply.github.com>
2018-12-16 18:45:29 -0500

    added nav bar to service provider page
    added nav bar to service provider page

--- src/components/serviceProvider.js
+++ src/components/serviceProvider.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import App from './App';
 import ClientCard from './ClientCard';
+import * as searchRes from './tempSearchRes';
 
 export class serviceProvider extends Component {
     render() {
         return (
-        <div>
+        <div>   
+            <searchRes.searchRes/>
             <ClientCard></ClientCard>
             <App></App>
             
