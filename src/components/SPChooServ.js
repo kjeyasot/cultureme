40d4f90ddda3522bb6e3cf362159d42528e0d2c8
nehal <n32gupta>
2019-03-08 01:35:56 -0500

    choose service

--- src/components/SPChooServ.js
+++ src/components/SPChooServ.js
@@ -59,7 +59,8 @@ export class SPChooseService extends Component {
       
       <div> 
       <navstuff.navstuff/>
-    
+      
+    <h2 className="Heading">Your Services</h2>
   <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet"/>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
