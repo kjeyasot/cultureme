c51a36cf49d2db4c6600e4f437cb05c8c9c456c3
ermsci342 <32082256+ermsci342@users.noreply.github.com>
2019-03-11 16:16:09 -0400

    my account page
    my account page

---
+++ src/components/myAccount.js
@@ -0,0 +1,170 @@
+import React, { Component } from 'react';
+import firebase, { auth, provider, storage, database  } from '../firebase.js';
+import * as footer1 from './footer-nav';
+import * as navstuff from './nav-boots';
+
+
+import * as script from '../scripts';
+const images = script.importAll(require.context('../ImagesOld', false, /\.(png|jpe?g|svg)$/));
+
+
+export class Upload extends React.Component {
+    constructor(){
+      super()
+      this.state = {
+        user: null,
+        file: null,
+        url: [],
+        images: []
+      }
+
+      this.handleChange = this.handleChange.bind(this)
+      this.storePhoto = this.storePhoto.bind(this)
+      this.deletePhoto = this.deletePhoto.bind(this)
+    }
+
+    
+    handleChange(e) {
+      this.setState({
+        file: e.target.files[0],
+        url: URL.createObjectURL(e.target.files[0])
+      })
+    }
+    storePhoto() {
+        auth.onAuthStateChanged((user) => {
+            if (user) {
+              this.setState({ user });
+      const key = database.ref().child('Photos').child(this.state.user.uid).push().key
+      const img = storage.ref().child('Images').child(this.state.user.uid).child(key)
+    
+    // WORKING FOR DB
+    img.put(this.state.file).then((snap) => {
+        // console.log('test'+ snap.metadata.downloadURLs)
+        storage.ref().child('Images').child(this.state.user.uid).child(img.name).getDownloadURL().then(url => {
+        database.ref().child('Photos').child(this.state.user.uid).child(key).set({
+          "url" : url
+        })
+      })
+      })
+      
+      this.setState({
+        file: null,
+        url: null,
+      })
+    }
+})
+}
+
+
+    deletePhoto(event) {
+      let uid = this.state.user.uid
+      let img = event.target.name
+      storage.ref().child('Images').child(uid).child(img).delete()
+      database.ref().child('Photos').child(this.state.user.uid).child(img).remove()
+
+    }
+
+    componentDidMount() {
+        auth.onAuthStateChanged((user) => {
+            if (user) {
+              this.setState({ user });
+      
+      const ref = database.ref().child('Photos').child(this.state.user.uid)
+      ref.on('child_added', (child) => {
+        let images = this.state.images.slice()
+        images.push({
+          key: child.key,
+          url: child.val().url
+        })
+        this.setState({images})
+      })
+      ref.on('child_removed', (child) => {
+        let images = this.state.images.filter((image) => {
+          return image.url != child.val().url
+        })
+        this.setState({images})
+      })
+    } 
+});
+    }
+    render() {
+      const previewStyle = {
+        maxHeight: "100px",
+        maxWidth: "100px",
+        float:'right'
+      }
+      const imgStyle = {
+        maxHeight: "150px",
+        maxWidth: "150px",
+        float:'middle',
+        paddingRight: "15px",
+        paddingBottom:"20px",
+      }
+
+      const isImage = this.state.file;
+      let imagePreview;
+
+      if (isImage) {
+        // imagePreview = <LogoutButton onClick={this.handleLogoutClick} />;
+        imagePreview = <img className="imageUploadSize" src={this.state.url }/>;
+      } else {
+        imagePreview = <img className="imageUploadSize" src={'http://via.placeholder.com/400x300'}/>
+      }
+
+      return (
+        <div>
+
+          <navstuff.navstuff/>
+      
+      <div class = "moveElements">
+        <div>
+            <div>
+            <h5 style={{float:'middle', fontFamily:"Arial"}}>Step 2: Upload Photos of Recent Work</h5>
+            {/* {this.state.user ?
+            <div>
+            {this.state.user.uid}</div>: null
+        } */}
+        <br></br>
+        </div>
+        <div>
+         <img className = "CoolGyalUpload" alt="sideImage" src={images['cool.png']}/>
+      </div>
+
+    
+          <input className = "btnupload" id="input" type="file" onChange={this.handleChange}/>
+          <br></br>
+          <br></br>
+          <div>
+            {imagePreview}
+          {/* <img className="imageUploadSize" src={'http://via.placeholder.com/400x300' || this.state.url }/> */}
+          </div>
+          <br></br>
+          <div class="upload-btn-wrapper">
+          <button className = "btnupload" onClick={this.storePhoto}>Upload</button>
+          </div>
+
+          {this.state.images.map((image) =>
+            <div key={image.key}>
+            <h1>{image.file}</h1>
+            <div class="row">
+                <div class="column">
+              <img src={image.url} style={imgStyle}/>
+              <button className = "removeButton" onClick={this.deletePhoto} 
+                 name={image.key}>X</button>
+
+                             </div>
+              </div>
+            </div>
+          )}
+          <div>
+          <button className = "doneButton" style={{float:'center'}}>Done</button>
+          </div>
+        </div>
+    </div>
+    {/* <div className="spfooter"> */}
+        <footer1.footer1/>
+        {/* </div> */}
+    </div>
+      );
+    }
+  }
