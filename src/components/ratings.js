99d2aa423e41b936ad2274749d7fab0497b2a723
ermsci342 <32082256+ermsci342@users.noreply.github.com>
2019-03-13 18:23:34 -0400

    ratings feature
    ratings feature

---
+++ src/components/ratings.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import firebase, { auth, provider, storage, database  } from '../firebase.js';
+import * as footer1 from './footer-nav';
+import * as navstuff from './nav-boots';
+import CryptoJS from "crypto-js";
+import StarRatingComponent from 'react-star-rating-component';
+
+
+import * as script from '../scripts';
+const images = script.importAll(require.context('../ImagesOld', false, /\.(png|jpe?g|svg)$/));
+let firstName;
+let lastName;
+let email;
+let companyName;
+let mobile;
+let postalCode;
+let password;
+let disabled;
+
+
+export class ratings extends React.Component {
+    constructor(){
+      super()
+      this.state = {
+        user: null,
+        file: null,
+        url: [],
+        images: [],
+        firstName: '',
+        lastName: '',
+        companyName: '',
+        email: '',
+        mobile: '',
+        postalCode: '',
+        password: '',
+        answer: '', 
+        disabled: "",
+        rating: 1
+      }
+    }
+
+
+    onStarClick(nextValue, prevValue, name) {
+      this.setState({rating: nextValue});
+    }
+
+    onStarHover(nextValue, prevValue, name) {
+      this.setState({rating: nextValue});
+    }
+
+    render() {
+
+      const { rating } = this.state;
+      return (
+        
+      <div className = "moveElements">
+      <h2>Rating from state: {rating}</h2>
+        <StarRatingComponent 
+          name="rate2" 
+          editing={false}
+          starCount={5}
+          value={3}
+        />
+
+
+
+        <div>
+        <h2>Rating from state: {rating}</h2>
+        <StarRatingComponent 
+          name="rate1" 
+          starCount={5}
+          value={rating}
+          onStarClick={this.onStarClick.bind(this)}
+        />
+      </div>
+  
+       </div> 
+      );
+    }
+  }
