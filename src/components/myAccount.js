e500697f0eddb239e42ceef55f8c312b561ec8dd
Karthigha J <32119141+kjeyasot@users.noreply.github.com>
2019-03-14 22:54:11 -0400

    account page -final fix

--- src/components/myAccount.js
+++ src/components/myAccount.js
@@ -5,8 +5,8 @@ import * as navstuff from './nav-boots';
 import CryptoJS from "crypto-js";
 
 
-import * as script from '../scripts';
-const images = script.importAll(require.context('../ImagesOld', false, /\.(png|jpe?g|svg)$/));
+// import * as script from '../scripts';
+// const images = script.importAll(require.context('../ImagesOld', false, /\.(png|jpe?g|svg)$/));
 let firstName;
 let lastName;
 let email;
@@ -14,7 +14,6 @@ let companyName;
 let mobile;
 let postalCode;
 let password;
-
 
 export class myAccount extends React.Component {
     constructor(){
@@ -31,57 +30,47 @@ export class myAccount extends React.Component {
         mobile: '',
         postalCode: '',
         password: '',
-        answer: '', 
-        isInEditMode: false
+        isInEditMode:false
       }
       this.userUpdate = this.userUpdate.bind(this);
       this.handleChange = this.handleChange.bind(this);
-      this.keepOrgValue = this.keepOrgValue.bind(this);
       this.userIntUpdate = this.userIntUpdate.bind(this);
-
-
-
+      this.Activate = this.Activate.bind(this);
     }
+    Activate() {
+      this.setState ({
+        isInEditMode: !this.state.isInEditMode
+   
+      });
+     }
     handleSubmit = (event) => {
-      this.setState({ answer: event.target.name });
       event.preventDefault();
-      // this.userUpdate();
       }
       handleChange(e) {
-        this.setState({
-          [e.target.name]: e.target.value
-        });
-      }
-      async keepOrgValue() {
-        await auth.onAuthStateChanged((user) => {
-        if (user) {
-        const servPV = {
-          // firstName: this.state.firstName,
-          // lastName: this.state.lastName,
-          companyName: this.state.companyName,
-          // email: this.state.email,
-          // mobile: phone,
-          // postalCode: this.state.postalCode,
-          // password: pw,
+        // console.log(e.target.name)
+        if(e.target.name==='mobile'){
+          const mobile = (e.target.validity.valid) ? e.target.value : this.state.mobile;
+          this.setState({ mobile});
+
         }
-        const serviceProvidersRef = firebase.database().ref('serviceProviders').child(user.uid);
 
-        serviceProvidersRef.once('value', (snapshot) => {
-            snapshot.child('PersonalInformation').forEach((personalInfo) => {
-              console.log(personalInfo)   
-              personalInfo.ref.update(servPV)  
-              window.location.reload(true);
-           
+        if(e.target.name==='companyName'){
+          const companyName = (e.target.validity.valid) ? e.target.value : this.state.companyName;
+          this.setState({ companyName});
 
-            });
-        })
+        }
+        // if(e.target.name==='password'){
+        //   this.setState({
+        //     [e.target.name]: e.target.value
+        //   });
+
+        // }
       }
-    });
-    // window.location.reload();
-      }
+
     componentDidMount() {
       auth.onAuthStateChanged((user) => {
         if (user) {
+          
           this.setState({ user });
           const serviceProvidersRef = firebase.database().ref('serviceProviders').child(user.uid);
 
@@ -90,7 +79,6 @@ export class myAccount extends React.Component {
                 let persInfo = personalInfo.val();
                 const test = CryptoJS.AES.decrypt(persInfo.password.toString(), 'secret key 123');
                 const pw = test.toString(CryptoJS.enc.Utf8);
-                // testemail.push(persInfo.firstName);
                 firstName = persInfo.firstName;
                 lastName = persInfo.lastName;
                 email = persInfo.email;
@@ -123,28 +111,36 @@ export class myAccount extends React.Component {
 
       auth.onAuthStateChanged((user) => {
         if (user) {
+          const tempPw = CryptoJS.AES.encrypt(this.state.password, 'secret key 123');
+          // const test = CryptoJS.AES.decrypt(tempPw.toString(), 'secret key 123');
+          // const pw = test.toString(CryptoJS.enc.Utf8);
+          const pw = tempPw.toString();
+          const phone = this.state.mobile.split(" ").join("");
+  
           const servPV = {
-            // firstName: this.state.firstName,
-            // lastName: this.state.lastName,
-            companyName: this.state.companyName1,
-            // email: this.state.email,
-            // mobile: phone,
-            // postalCode: this.state.postalCode,
-            // password: pw,
+            companyName: this.state.companyName,
+            mobile: phone,
+            postalCode: this.state.postalCode,
+            password: pw,
           }
-          console.log(this.state.companyName1)
+          // console.log(this.state.companyName1)
           const serviceProvidersRef = firebase.database().ref('serviceProviders').child(user.uid);
 
           serviceProvidersRef.once('value', (snapshot) => {
               snapshot.child('PersonalInformation').forEach((personalInfo) => {
                 console.log(personalInfo)   
                 personalInfo.ref.update(servPV)             
-               window.location.reload(true);
+              //  window.location.reload(true);
 
               });
           })
+          
               }
       })
+      this.setState ({
+        isInEditMode: false
+   
+      });
             }
       
       userIntUpdate() {
@@ -178,13 +174,15 @@ export class myAccount extends React.Component {
                   mobile: mobile,
                   postalCode: postalCode,
                   password: password,
+                  isInEditMode: false
                 })
             });
   
           } 
           
+          
         });
-
+       
               }
     render() {
       return (
@@ -220,21 +218,29 @@ export class myAccount extends React.Component {
                 &nbsp;&nbsp;
                <input  className = 'noEdit' type="text" name="email" value = {this.state.user.email} disabled="disabled"/>
               </h6>
+              <h6>
               <br></br>
+                Password:
+                &nbsp;&nbsp;
+                &nbsp;&nbsp;
 
-              {this.state.isInEditMode ?
-                this.renderEditview()  :
-                this.renderDefaultview()
-                }
-              {/* <form onSubmit={this.handleSubmit}>
+                <input className = "noEdit" type="password" name="password" value={this.state.password} disabled="disabled"/>
+                  
+
+               &nbsp;&nbsp;
+             
+               
+              </h6>
+              <br></br>
+              <form onSubmit={this.handleSubmit}>
               <div>
               <h5 style={{float:'middle', fontFamily:"Arial", color:"palevioletred"}}>Service Information</h5>
               
               <div className = 'editButtons'>
               
                &nbsp;&nbsp;
-               {this.state.answer === "yes" ? <button className = "fa fa-times" onClick= {this.keepOrgValue} type = 'submit'></button> 
-               : <button  name = "yes" className = "fa fa-edit"  onClick={this.handleSubmit}></button>}
+                {this.state.isInEditMode?<button className = "fa fa-times" onClick= {this.userIntUpdate} type = 'submit'></button> : 
+                <button  name = "yes" className = "fa fa-edit"  onClick={this.Activate}></button> }
                </div>
                </div>
               <br></br>
@@ -244,8 +250,8 @@ export class myAccount extends React.Component {
                 &nbsp;&nbsp;
 
 
-              {this.state.answer === "yes"? <input className = "editAccount" type="text" name="companyName1" onChange={this.handleChange} defaultValue ={this.state.companyName}value={this.state.companyName1}/> :
-               <input type="text" name="companyName" defaultValue={this.state.companyName} disabled="disabled"/>}
+              {this.state.isInEditMode? <input className = "editAccount" type="text" name="companyName" pattern ="[A-Za-z]*"onChange={this.handleChange} value ={this.state.companyName}/> :
+               <input type="text" name="companyName" value={this.state.companyName} disabled="disabled"/>}
               
               </h6>
               <br></br>
@@ -259,7 +265,8 @@ export class myAccount extends React.Component {
                 &nbsp;&nbsp;
                 &nbsp;&nbsp;
                
-               {this.state.answer === "yes" ? <input className = "editAccount" type="text" name="contact1" defaultValue={this.state.mobile}/> : <input type="text" name="contact" defaultValue={this.state.mobile} disabled="disabled"/>}
+               {this.state.isInEditMode ? <input className = "editAccount" type="text" name="mobile" pattern="[0-9]*" maxLength= "10" onChange={this.handleChange} value={this.state.mobile}/> :
+                <input type="text" name="mobile" value={this.state.mobile} disabled="disabled"/>}
               </h6>
               <br></br>
 
@@ -270,31 +277,33 @@ export class myAccount extends React.Component {
                 &nbsp;&nbsp;
                 &nbsp;&nbsp;
                 &nbsp;&nbsp;
-                {this.state.answer === "yes" && <input className = "editAccount" type="text" name="location"  defaultValue={this.state.postalCode}/>|| <input type="text" name="location"  defaultValue={this.state.postalCode} disabled="disabled"/>}
+                {this.state.isInEditMode? <input className = "editAccount" type="text" name="postalCode" onChange={this.handleChange} defaultValue={this.state.postalCode}/>:
+                 <input type="text" name="location"  value={this.state.postalCode} disabled="disabled"/>}
                
                
               </h6>
               <br></br>
 
                 <h6>
-                Password:
+                {/* Password:
                 &nbsp;&nbsp;
                 &nbsp;&nbsp;
                 &nbsp;&nbsp;
                 &nbsp;&nbsp;
 
-                 {this.state.answer === "yes" && <input className = "editAccount" type="password" name="password" defaultValue={this.state.password}/>|| <input type="password" name="password" defaultValue={this.state.password} disabled="disabled"/>}
-               
+                 {this.state.isInEditMode? <input className = "editAccount" type="password" name="password" onChange={this.handleChange} value={this.state.password}/>:
+                  <input type="password" name="password" value={this.state.password} disabled="disabled"/>} */}
+
                &nbsp;&nbsp;
                <br></br>
                <br></br>
                <div className = 'editButtons'>
-               {this.state.answer === "yes" && <button className = "fa fa-save" type= 'submit' onClick={this.userUpdate}></button>}
+               {this.state.isInEditMode?  <button className = "fa fa-save" type= 'submit' onClick={this.userUpdate}></button>: null}
 
                </div>
                
-              </h6>
-              </form> */}
+              </h6> 
+              </form>
 
             </form>
             
