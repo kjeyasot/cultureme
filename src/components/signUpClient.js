49c932cb5cf5f43297bb9cd27ccfd05886639701
Sana Irfan <s5irfan@uwaterloo.ca>
2019-03-17 00:21:02 -0400

    sign in UI

--- src/components/signUpClient.js
+++ src/components/signUpClient.js
@@ -5,6 +5,8 @@ import { Link } from 'react-router-dom';
 import firebase, { auth, provider } from '../firebase.js';
 import CryptoJS from "crypto-js";
 // var CryptoJS = require("crypto-js");
+import { MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardBody, MDBInput, MDBBtn, MDBIcon, MDBModalFooter,MDBView } from 'mdbreact';
+
 
 const images = script.importAll(require.context('../ImagesOld', false, /\.(png|jpe?g|svg)$/));
 const emailReg = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i;
@@ -97,56 +99,125 @@ export class signUpClient extends Component {
   render() {
       
       return (
-        <div>
-        <img className="backgroundImg" alt="img" src={images['wed2.png']} />
-        <div id = "myModal" className="signUpMainModal">
-            <div className='signUpPageBg'>
-            <div className="signUpPageFields">
+        <MDBView className="viewClient">
+        <MDBContainer>
+      <MDBRow>
+        <MDBCol md="6">
+          <MDBCard style={{ marginTop: "5vh"}}>
+            <MDBCardBody className="mx-1" >
+              <div className="text-center">
+              <img className="logosignup" alt="img" src={images['blackLogo.png']} />
+                <h3 className="dark-grey-text mb-8">
+                  <strong>Sign Up</strong>
+                </h3>
+              </div>
+              <MDBInput
+                label="First Name"
+                group
+                type="text"
+                validate
+                error="wrong"
+                success="right"
+                style={{ paddingBottom: "3vh"}}
+                name='firstName'
+                onChange={this.handleChange} value={this.state.firstName}
+              />
 
-            <p2> Sign Up</p2><br></br>
-            <form onSubmit={this.handleSubmit}>
-            <input className='field' id = 'first'type = 'text' name='firstName' placeholder = 'First Name' onChange={this.handleChange} value={this.state.firstName}/><br></br>
-            {(this.state.firstName && !this.state.firstName.match(letters))? <p id="letter" className="invalid">Invalid First Name</p>:null}
-            
-            <input className='field' id = 'last'type = 'text' name='lastName' placeholder = 'Last Name' onChange={this.handleChange} value={this.state.lastName}/><br></br>
-            {(this.state.lastName && !this.state.lastName.match(letters))? <p id="letter" className="invalid">Invalid Last Name</p>:null}
-            
-            <input className='field' id = 'email'type = 'text' name='email' placeholder = 'E-mail' onChange={this.handleChange} value={this.state.email}/><br></br>
-            {(this.state.email && !this.state.email.match(emailReg))? <p id="letter" className="invalid">Invalid <b>E-mail</b> Address (ex: abc@abc.com)</p>:null}
-            {(this.state.email && testemail.indexOf(this.state.email)>-1)? <p id="letter" className="invalid">E-mail Address Already Exists</p> : null}
-            
-            <input className='field' id = 'password'type = 'password' name='password' placeholder = 'Password' onChange={this.handleChange} value={this.state.password}/><br></br>
-            { this.state.password ? <p className="pwWarning">Password must contain the following:</p> : null}
-        
-            {(this.state.password && this.state.password.match(/[a-z]/g))?<p id="letter" className="valid">A <b>lowercase</b> letter</p>:null}
-            {(this.state.password && !this.state.password.match(/[a-z]/g))?<p id="letter" className="invalid">A <b>lowercase</b> letter</p>:null}
-
-            {(this.state.password && this.state.password.match(/[A-Z]/g))?<p id="capital" class="valid">A <b>capital (uppercase)</b> letter</p>:null}
-            {(this.state.password && !this.state.password.match(/[A-Z]/g))?<p id="capital" class="invalid">A <b>capital (uppercase)</b> letter</p>:null}
-
-            {(this.state.password && this.state.password.match(/[0-9]/g))? <p id="number" class="valid">A <b>number</b></p>:null}
-            {(this.state.password && !this.state.password.match(/[0-9]/g))?<p id="number" class="invalid">A <b>number</b></p>:null}
-
-            {(this.state.password && this.state.password.length>=8)? <p id="length" class="valid">Minimum <b>8 characters</b></p>:null}
-            {(this.state.password && this.state.password.length<8)?<p id="length" class="invalid">Minimum <b>8 characters</b></p>:null}
-
-            
-            <input className = 'submitBtn' type= 'submit' value= 'Sign Up'
-            disabled={!this.state.firstName||!this.state.firstName.match(letters)||!this.state.lastName||!this.state.lastName.match(letters)||!this.state.email.match(emailReg)||
-              testemail.indexOf(this.state.email)>-1||!this.state.password||!this.state.password.match(/[a-z]/g)||!this.state.password.match(/[A-Z]/g)||
-              !this.state.password.match(/[0-9]/g)|| this.state.password.length<8}
+               {(this.state.firstName && 
+                !this.state.firstName.match(letters))? 
+                <p id="letter" className="invalid">Invalid First Name</p>:null}
               
-            /><br></br>
-            </form>
-          <p1>Already have an account?</p1>
-          <Link to="/signin">   
-                <u id="signInTxt"> Sign In</u>
-            </Link>
-            </div>
-            </div>
-            </div>
+              <MDBInput
+                label="Last Name"
+                group
+                type="text"
+                validate
+                error="wrong"
+                success="right"
+                name='lastName'
+                id = 'last'
+                onChange={this.handleChange} value={this.state.lastName}
+                style={{ paddingBottom: "3vh"}}
+              />
+               {(this.state.lastName && !this.state.lastName.match(letters))? <p id="letter" className="invalid">Invalid Last Name</p>:null}
 
-        </div>
+
+
+              <MDBInput
+                label="Email"
+                group
+                type="email"
+                validate
+                error="wrong"
+                success="right"
+                name='email'
+                id = 'email'
+                onChange={this.handleChange} value={this.state.email}
+                style={{ paddingBottom: "3vh"}}
+              />
+              {(this.state.email && !this.state.email.match(emailReg))? <p id="letter" className="invalid">Invalid <b>E-mail</b> Address (ex: abc@abc.com)</p>:null}
+              {(this.state.email && testemail.indexOf(this.state.email)>-1)? <p id="letter" className="invalid">E-mail Address Already Exists</p> : null}
+
+            
+              
+              <MDBInput
+                label="Your password"
+                group
+                type="password"
+                validate
+                containerClass="mb-0"
+                name='firstName'
+                id = 'password'
+                name='password'
+                onChange={this.handleChange} value={this.state.password}
+                style={{ paddingBottom: "3vh"}}
+              />
+              {/* { this.state.password ? <p className="pwWarning">Password must contain the following:</p> : null} */}
+        
+        {/* {(this.state.password && this.state.password.match(/[a-z]/g))?<p id="letter" className="valid">A <b>lowercase</b> letter</p>:null} */}
+        {(this.state.password && !this.state.password.match(/[a-z]/g))?<p id="letter" className="invalid">A <b>lowercase</b> letter</p>:null}
+
+        {/* {(this.state.password && this.state.password.match(/[A-Z]/g))?<p id="capital" class="valid">A <b>capital (uppercase)</b> letter</p>:null} */}
+        {(this.state.password && !this.state.password.match(/[A-Z]/g))?<p id="capital" class="invalid">A <b>capital (uppercase)</b> letter</p>:null}
+
+        {/* {(this.state.password && this.state.password.match(/[0-9]/g))? <p id="number" class="valid">A <b>number</b></p>:null} */}
+        {(this.state.password && !this.state.password.match(/[0-9]/g))?<p id="number" class="invalid">A <b>number</b></p>:null}
+
+        {/* {(this.state.password && this.state.password.length>=8)? <p id="length" class="valid">Minimum <b>8 characters</b></p>:null} */}
+        {(this.state.password && this.state.password.length<8)?<p id="length" class="invalid">Minimum <b>8 characters</b></p>:null}
+                <br></br>    
+             
+              <div className="text-center mb-3">
+                <MDBBtn onClick={this.signup}
+                  type="submit"
+                  gradient="ripe-malinka"
+                  rounded
+                  className="btn-block z-depth-1a"
+                  id='submitBtn' 
+                  disabled={!this.state.firstName||!this.state.firstName.match(letters)||!this.state.lastName||!this.state.lastName.match(letters)||!this.state.email.match(emailReg)||
+                    testemail.indexOf(this.state.email)>-1||!this.state.password||!this.state.password.match(/[a-z]/g)||!this.state.password.match(/[A-Z]/g)||
+                    !this.state.password.match(/[0-9]/g)|| this.state.password.length<8}
+                >
+                  Sign Up
+                </MDBBtn>
+              </div>
+
+               <p className="font-small grey-text d-flex justify-content-end pb-4">
+             <span>Already a member?</span> 
+             <Link to="/signin">   
+                <a href="#!" className="blue-text ml-1">
+                  Sign In
+                </a>
+                </Link>
+              </p>
+    
+            </MDBCardBody>
+          
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+    </MDBView>
     );
   }
 }
--- src/components/signUpSP.js
+++ src/components/signUpSP.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import firebase, { auth, provider } from '../firebase.js';
 import CryptoJS from "crypto-js";
 import { error } from 'util';
+import { MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardBody, MDBInput, MDBBtn, MDBIcon, MDBModalFooter,MDBView } from 'mdbreact';
 
 const images = script.importAll(require.context('../ImagesOld', false, /\.(png|jpe?g|svg)$/));
 const emailReg = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i;
@@ -14,6 +15,8 @@ const letters = /^[A-Za-z]+$/;
 let testemail = [];
 let testCompany = [];
 let testPhone = [];
+
+const images1 = script.importAll(require.context('../ImagesOld', false, /\.(png|jpe?g|svg)$/));
 
 export class signUpSP extends Component {
     constructor() {
@@ -39,7 +42,7 @@ export class signUpSP extends Component {
       }
       handleSubmit(e) {
         e.preventDefault();
-        this.signup();
+        // this.signup();
         // this.props.history.push('/choose-service')
       }
     
@@ -112,69 +115,156 @@ export class signUpSP extends Component {
   render() {
       
       return (
+        
+
+
+        <MDBView>
+        <MDBContainer>
+      <MDBRow>
+        <MDBCol md="6">
+          <MDBCard style={{ marginTop: "0.01vh"}}>
+            <MDBCardBody className="mx-1" >
+              <div className="text-center">
+              <img className="logosignup" alt="img" src={images['blackLogo.png']} />
+                <h3 className="dark-grey-text mb-8">
+                  <strong>Sign Up</strong>
+                </h3>
+              </div>
+              <MDBInput
+                label="First Name"
+                group
+                type="text"
+                validate
+                error="wrong"
+                success="right"
+                style={{ paddingBottom: "3vh"}}
+                name='firstName'
+                onChange={this.handleChange} value={this.state.firstName}
+              />
+
+               {(this.state.firstName && 
+                !this.state.firstName.match(letters))? 
+                <p id="letter" className="invalid">Invalid First Name</p>:null}
+              
+              <MDBInput
+                label="Last Name"
+                group
+                type="text"
+                validate
+                error="wrong"
+                success="right"
+                name='lastName'
+                id = 'last'
+                onChange={this.handleChange} value={this.state.lastName}
+                style={{ paddingBottom: "3vh"}}
+              />
+               {(this.state.lastName && !this.state.lastName.match(letters))? <p id="letter" className="invalid">Invalid Last Name</p>:null}
+
+              <MDBInput
+                label="Company Name"
+                group
+                type="text"
+                validate
+                error="wrong"
+                success="right"
+                name='companyName'
+                id = 'companyName'
+                onChange={this.handleChange} value={this.state.companyName}
+                style={{ paddingBottom: "3vh"}}
+                maxlength=""
+              />
+
+                {(this.state.companyName && testCompany.indexOf(this.state.companyName)>-1)? <p id="letter" className="invalid">Company Name Already Exists</p> : null}
+
+
+              <MDBInput
+                label="Email"
+                group
+                type="email"
+                validate
+                error="wrong"
+                success="right"
+                name='email'
+                id = 'email'
+                onChange={this.handleChange} value={this.state.email}
+                style={{ paddingBottom: "3vh"}}
+              />
+              {(this.state.email && !this.state.email.match(emailReg))? <p id="letter" className="invalid">Invalid <b>E-mail</b> Address (ex: abc@abc.com)</p>:null}
+              {(this.state.email && testemail.indexOf(this.state.email)>-1)? <p id="letter" className="invalid">E-mail Address Already Exists</p> : null}
+
+              <MDBInput
+                label="Phone Number"
+                group
+                type="text"
+                validate
+                containerClass="mb-0"
+                name='firstName'
+                style={{ paddingBottom: "3vh"}}
+                id = 'mobile'
+                name='mobile'
+                onChange={this.handleChange} value={this.state.mobile}
+              />
+              {(this.state.mobile && !this.state.mobile.match(phoneReg))? <p id="letter" className="invalid">Invalid <b>Phone</b> Number (ex: 111 111 1111)</p>:null}
+              {(this.state.mobile && ((testPhone.indexOf(this.state.mobile)>-1)||(testPhone.indexOf(this.state.mobile.split(" ").join(""))>-1)))? <p id="letter" className="invalid">Phone Number Already Exists</p> : null}  
+              
+              <MDBInput
+                label="Your password"
+                group
+                type="password"
+                validate
+                containerClass="mb-0"
+                name='firstName'
+                id = 'password'
+                name='password'
+                onChange={this.handleChange} value={this.state.password}
+                style={{ paddingBottom: "3vh"}}
+              />
+              {/* { this.state.password ? <p className="pwWarning">Password must contain the following:</p> : null} */}
+        
+        {/* {(this.state.password && this.state.password.match(/[a-z]/g))?<p id="letter" className="valid">A <b>lowercase</b> letter</p>:null} */}
+        {(this.state.password && !this.state.password.match(/[a-z]/g))?<p id="letter" className="invalid">A <b>lowercase</b> letter</p>:null}
+
+        {/* {(this.state.password && this.state.password.match(/[A-Z]/g))?<p id="capital" class="valid">A <b>capital (uppercase)</b> letter</p>:null} */}
+        {(this.state.password && !this.state.password.match(/[A-Z]/g))?<p id="capital" class="invalid">A <b>capital (uppercase)</b> letter</p>:null}
+
+        {/* {(this.state.password && this.state.password.match(/[0-9]/g))? <p id="number" class="valid">A <b>number</b></p>:null} */}
+        {(this.state.password && !this.state.password.match(/[0-9]/g))?<p id="number" class="invalid">A <b>number</b></p>:null}
+
+        {/* {(this.state.password && this.state.password.length>=8)? <p id="length" class="valid">Minimum <b>8 characters</b></p>:null} */}
+        {(this.state.password && this.state.password.length<8)?<p id="length" class="invalid">Minimum <b>8 characters</b></p>:null}
+                <br></br>    
+             
+              <div className="text-center mb-3">
+                <MDBBtn onClick={this.signup}
+                  type="submit"
+                  gradient="ripe-malinka"
+                  rounded
+                  className="btn-block z-depth-1a"
+                  id='submitBtn' 
+                  disabled={!this.state.firstName||!this.state.firstName.match(letters)||!this.state.lastName||!this.state.lastName.match(letters)||!this.state.companyName||testCompany.indexOf(this.state.companyName)>-1||!this.state.email.match(emailReg)||
+                    testemail.indexOf(this.state.email)>-1||!this.state.mobile.match(phoneReg)||testPhone.indexOf(this.state.mobile)>-1||testPhone.indexOf(this.state.mobile.split(" ").join(""))>-1||!this.state.password
+                  ||!this.state.password.match(/[a-z]/g)||!this.state.password.match(/[A-Z]/g)||!this.state.password.match(/[0-9]/g)|| this.state.password.length<8}
+                >
+                  Sign Up
+                </MDBBtn>
+              </div>
+
+               <p className="font-small grey-text d-flex justify-content-end pb-4">
+             <span>Already a member?</span> 
+             <Link to="/signInSP">   
+                <a href="#!" className="blue-text ml-1">
+                  Sign In
+                </a>
+                </Link>
+              </p>
+    
+            </MDBCardBody>
           
-          <div>
-        <img className="backgroundImg" alt="img" src={images['wed2.png']} />
-        <div id = "myModal" className="signUpMainModal">
-        <div className='signUpPageBg'>
-        <div className="signUpPageFields">
-
-        <p2> Sign Up</p2><br></br>
-        <form onSubmit={this.handleSubmit}> 
-            <input className='field' id = 'first'type = 'text' name='firstName' placeholder = 'First Name' onChange={this.handleChange} value={this.state.firstName}/><br></br>
-            {(this.state.firstName && !this.state.firstName.match(letters))? <p id="letter" className="invalid">Invalid First Name</p>:null}
-            
-            <input className='field' id = 'last'type = 'text' name='lastName' placeholder = 'Last Name' onChange={this.handleChange} value={this.state.lastName}/><br></br>
-            {(this.state.lastName && !this.state.lastName.match(letters))? <p id="letter" className="invalid">Invalid Last Name</p>:null}
-
-            <input className='field' id = 'companyName'type = 'text' name='companyName' placeholder = 'Company Name' onChange={this.handleChange} value={this.state.companyName}/><br></br>
-            {(this.state.companyName && testCompany.indexOf(this.state.companyName)>-1)? <p id="letter" className="invalid">Company Name Already Exists</p> : null}
-
-
-            <input className='field' id = 'email'type = 'email' name='email' placeholder = 'E-mail'  onChange={this.handleChange} value={this.state.email}/><br></br>
-            {(this.state.email && !this.state.email.match(emailReg))? <p id="letter" className="invalid">Invalid <b>E-mail</b> Address (ex: abc@abc.com)</p>:null}
-            {(this.state.email && testemail.indexOf(this.state.email)>-1)? <p id="letter" className="invalid">E-mail Address Already Exists</p> : null}
-
-            <input className='field' id = 'mobile'type = 'text' name='mobile' placeholder = 'Contact' onChange={this.handleChange} value={this.state.mobile}/><br></br>
-            {(this.state.mobile && !this.state.mobile.match(phoneReg))? <p id="letter" className="invalid">Invalid <b>Phone</b> Number (ex: 111 111 1111)</p>:null}
-            {(this.state.mobile && ((testPhone.indexOf(this.state.mobile)>-1)||(testPhone.indexOf(this.state.mobile.split(" ").join(""))>-1)))? <p id="letter" className="invalid">Contact Number Already Exists</p> : null}
-
-
-            <input className='field' id = 'postalCode' type = 'text' name='postalCode' placeholder = 'Postal Code' onChange={this.handleChange} value={this.state.postalCode}/><br></br>
-            {(this.state.postalCode && !this.state.postalCode.match(postReg))? <p id="letter" className="invalid">Invalid <b>Postal Code</b> (ex: M1B 3B6)</p>:null}
-                      
-            <input className='field' id = 'password'type = 'password' name='password' placeholder = 'Password' onChange={this.handleChange} value={this.state.password}/><br></br>
-            { this.state.password ? <p className="pwWarning">Password must contain the following:</p> : null}
-        
-            {(this.state.password && this.state.password.match(/[a-z]/g))?<p id="letter" className="valid">A <b>lowercase</b> letter</p>:null}
-            {(this.state.password && !this.state.password.match(/[a-z]/g))?<p id="letter" className="invalid">A <b>lowercase</b> letter</p>:null}
-
-            {(this.state.password && this.state.password.match(/[A-Z]/g))?<p id="capital" class="valid">A <b>capital (uppercase)</b> letter</p>:null}
-            {(this.state.password && !this.state.password.match(/[A-Z]/g))?<p id="capital" class="invalid">A <b>capital (uppercase)</b> letter</p>:null}
-
-            {(this.state.password && this.state.password.match(/[0-9]/g))? <p id="number" class="valid">A <b>number</b></p>:null}
-            {(this.state.password && !this.state.password.match(/[0-9]/g))?<p id="number" class="invalid">A <b>number</b></p>:null}
-
-            {(this.state.password && this.state.password.length>=8)? <p id="length" class="valid">Minimum <b>8 characters</b></p>:null}
-            {(this.state.password && this.state.password.length<8)?<p id="length" class="invalid">Minimum <b>8 characters</b></p>:null}
-                        
-            <input id='submitBtn' className = 'submitBtn' type= 'submit' value= 'Sign Up' 
-            disabled={!this.state.firstName||!this.state.firstName.match(letters)||!this.state.lastName||!this.state.lastName.match(letters)||!this.state.companyName||testCompany.indexOf(this.state.companyName)>-1||!this.state.email.match(emailReg)||
-            testemail.indexOf(this.state.email)>-1||!this.state.mobile.match(phoneReg)||testPhone.indexOf(this.state.mobile)>-1||testPhone.indexOf(this.state.mobile.split(" ").join(""))>-1||!this.state.postalCode.match(postReg)||!this.state.password
-          ||!this.state.password.match(/[a-z]/g)||!this.state.password.match(/[A-Z]/g)||!this.state.password.match(/[0-9]/g)|| this.state.password.length<8}
-            /><br></br>
-            </form>
-        
-        <p1>Already have an account?</p1>
-        <Link to="/signInSP">   
-            <u id="signInTxt"> Sign In</u>
-        </Link>
-            </div>
-            </div>
-            </div>
-
-        </div>
-        
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+    </MDBView>
     );
   }
   
