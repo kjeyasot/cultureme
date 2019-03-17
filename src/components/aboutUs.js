c56d2262165126f69b7d45389ce924d29dcf1fa9
ermsci342 <32082256+ermsci342@users.noreply.github.com>
2019-03-16 21:53:45 -0400

    about us page
    about us page

--- src/components/aboutUs.js
+++ src/components/aboutUs.js
@@ -4,6 +4,9 @@ import * as script from '../scripts';
 import { Link } from 'react-router-dom';
 import firebase, { auth, provider } from '../firebase.js';
 import CryptoJS from "crypto-js";
+import * as footer1 from './footer-nav';
+import * as navstuff from './nav-boots';
+import {  MDBRow, MDBCol, MDBCard, MDBCardBody, MDBIcon } from "mdbreact";
 
 const images = script.importAll(require.context('../ImagesOld', false, /\.(png|jpe?g|svg)$/));
 
@@ -33,16 +36,73 @@ export class aboutUs extends Component {
 
   render() {
       return (
+        <div>
+        <navstuff.navstuff/>
+<MDBCard className="text-center">
+        <MDBCardBody>
+          <h2 style={{color: 'black', fontFamily: 'Arial', fontWeight: 'bold', fontSize: '3.5vw'}}>About Us</h2>
+          <p style={{fontSize: "2vw", fontStyle: 'Arial'}} className="grey-text w-responsive mx-auto mb-1">
+           CultureMe connects service providers to individuals seeking cultural services.
+The application allows service providers to showcase their portfolio and effectively outreach to potential
+clients. Through an optimized search engine, the platform aims to help people find the most relevant service
+with ease. 
+          </p>
+<br></br>
+          <iframe width="50%" height="250vh" src="https://www.youtube.com/embed/IEAW8Bh5Gcg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          
+          <br></br>
+          <br></br>
 
-        <section class="mb-4">
+          <MDBRow>
+            <MDBCol lg="3" md="6" className="mb-lg-0 mb-5">
+            <img src= 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909__340.png' className="img-circle z-depth-1"/>
+              <h5 style={{colour: 'black', fontStyle: 'arial',fontSize: '2vw'}} className="font-weight-bold mt-4 mb-3">Sana Irfan</h5>
+              <p style={{color: 'palevioletred', fontStyle: 'arial', fontSize: '1.5vw'}}>Product Manager</p>
+              <ul className="list-unstyled mb-0">
+                <a href="#!" className="p-2 fa-lg">
+                  <MDBIcon fab icon="linkedin" className="black-text" />
+                </a>
+              </ul>
+            </MDBCol>
 
-        <h2 class="h1-responsive font-weight-bold text-center my-4">Contact us</h2>
-    <p class="text-center w-responsive mx-auto mb-5">Do you have any questions? Please do not hesitate to contact us directly. Our team will come back to you within
-        a matter of hours to help you.</p>
-        
-        
-        </section>
+                        <MDBCol lg="3" md="6" className="mb-lg-0 mb-5">
+                        <img src= 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909__340.png' className="img-circle z-depth-1"/>
+              <h5 style={{colour: 'grey', fontStyle: 'arial',fontSize: '2vw'}} className="font-weight-bold mt-4 mb-3">Karthigha Jeyasotharan</h5>
+              <p style={{color: 'palevioletred', fontStyle: 'arial', fontSize: '1.5vw'}}>Backend Devloper</p>
+              <ul className="list-unstyled mb-0">
+                <a href="#!" className="p-2 fa-lg">
+                  <MDBIcon fab icon="linkedin" className="black-text" />
+                </a>
+              </ul>
+            </MDBCol>
 
+
+                        <MDBCol lg="3" md="6" className="mb-lg-0 mb-5">
+                        <img src= 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909__340.png' className="img-circle z-depth-1"/>
+              <h5 style={{colour: 'black', fontStyle: 'arial',fontSize: '2vw'}} className="font-weight-bold mt-4 mb-3">Nehal Gupta</h5>
+              <p style={{color: 'palevioletred', fontStyle: 'arial', fontSize: '1.5vw'}}>Backend Developer</p>
+              <ul className="list-unstyled mb-0">
+                <a href="#!" className="p-2 fa-lg">
+                  <MDBIcon fab icon="linkedin" className="black-text" />
+                </a>
+              </ul>
+            </MDBCol>
+
+                        <MDBCol lg="3" md="6" className="mb-lg-0 mb-5">
+                        <img src= 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909__340.png' className="img-circle z-depth-1"/>
+              <h5 style={{colour: 'black', fontStyle: 'arial',fontSize: '2vw'}} className="font-weight-bold mt-4 mb-3">Eman Rashdi </h5>
+              <p style={{color: 'palevioletred', fontStyle: 'arial', fontSize: '1.5vw'}}>Frontend Developer</p>
+              <ul className="list-unstyled mb-0">
+                <a href="#!" className="p-2 fa-lg">
+                  <MDBIcon fab icon="linkedin" className="black-text" />
+                </a>
+              </ul>
+            </MDBCol>
+
+          </MDBRow>
+        </MDBCardBody>
+      </MDBCard>
+      </div>
     );
   }
 }
