01042a31e3ef507ece7190e11d5fe9e6e4b9d5cb
ermsci342 <32082256+ermsci342@users.noreply.github.com>
2019-03-18 18:07:04 -0400

    make temp page for account step 2
    make temp page for account step 2

--- src/components/Step2.js
+++ src/components/Step2.js
@@ -8,7 +8,7 @@ import { Link } from 'react-router-dom';
 
 import * as script from '../scripts';
 
-export class stepone extends Component {
+export class step2 extends Component {
   constructor() {
     super();
     this.state = {
