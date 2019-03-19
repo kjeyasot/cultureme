5c54268c042e16d9b0a1b25c3184e10de62ff8fa
ermsci342 <32082256+ermsci342@users.noreply.github.com>
2019-03-18 21:01:01 -0400

    view/edit page
    view/edit page

--- src/components/Step2.js
+++ src/components/Step2.js
@@ -5,6 +5,7 @@ import * as navstuff from './nav-boots';
 import firebase, { auth, provider, database } from '../firebase.js';
 import { Link } from 'react-router-dom';
 
+ 
 
 import * as script from '../scripts';
 
@@ -99,157 +100,198 @@ export class step2 extends Component {
 
   render(){
     const images = script.importAll(require.context('../ImagesOld', false, /\.(png|jpe?g|svg)$/));
- 
       
   return (
     <div>
-      
-      <navstuff.navstuff/>
-      <div class = "moveElements1">
-      <h3 style={{ fontSize: "2.5vmax",fontFamily:"Arial"}}>Let's get started with creating a service</h3>
-           <br></br>
-            <h5 style={{ fontSize: "2vmax",fontFamily:"Arial"}}>Step 1: What type of service do you want to provide</h5>
-    </div>
-  <br></br>
-
-  <div>
-         <img className = "CoolGyalstep1" alt="sideImage" src={images['cool.png']}/>
-      </div>
-  <MDBContainer>
-      <MDBRow>
-        <MDBCol md="10" text-center>
-          <form onSubmit={this.handleSubmit}>
-            <label
-              htmlFor="defaultFormCardNameEx"
-              className="black-text font-weight-light"
-            >
-              Service Type
-            </label>
-            <input
-              type="text"
-              id="defaultFormCardNameEx"
-              className="form-control"
-              name='serviceType' pattern ="[A-Za-z\s]*" maxlength="30"
-              onChange={this.handleChange} 
-              value={this.state.serviceType}
-            />
-           
-            <br />
-            <label
-              htmlFor="defaultFormCardEmailEx"
-              className="black-text font-weight-dark"
-            >
-              Descritption
-            </label>
-            <textarea
-              type="email"
-              id="defaultFormCardEmailEx"
-              className="form-control"
-              name='Description' 
-              onChange={this.handleChange} 
-              value={this.state.Description}
-            />
-<br></br>
-
-<label
-              htmlFor="defaultFormCardEmailEx"
-              className="black-text font-weight-dark"
-            >
-           Price
-            </label>
-          
-
-
-
-
-            <div class="row">
-
-    <div class="col">
-    <div className="input-group">
-      <div className="input-group-prepend">
-        <span className="input-group-text" id="basic-addon">
-          <i className="fa fa-dollar"></i>
-        </span>
-      </div>
-      <input class="form-control form-control-md " md="20" type="text"  placeholder="Min Price" aria-describedby="basic-addon" 
-      name='minPrice'  pattern="[0-9]*" maxlength="4"
-      onChange={this.handleChange} 
-      value={this.state.minPrice}/>
-     
-    </div>
-    </div>
-    <h3>-</h3>
-       <div class="col">
-    <div className="input-group">
-      <div className="input-group-prepend">
-        <span className="input-group-text" id="basic-addon">
-          <i className="fa fa-dollar"></i>
-        </span>
-      </div>
-      <input type="text" className="form-control  form-control-md" placeholder="Max Price" aria-label="Username" aria-describedby="basic-addon" 
-      name='maxPrice'  pattern="[0-9]*"  maxlength="4"
-      onChange={this.handleChange} 
-      value={this.state.maxPrice}/>
-      
-        </div>
-        </div>
-  </div>
-  <br></br>   
-            {( this.state.minPrice && this.state.maxPrice && Number(this.state.minPrice)>= Number(this.state.maxPrice))? <p id="letter" className="invalid">Invalid Price Range</p>:null}
-      
-<br></br>
-<label
-              htmlFor="defaultFormCardNameEx"
-              className="black-text font-weight-light"
-            >
-              Location
-            </label>
-            <br />
-           
-            <div class="row">
-           
-            <div class="col">
-      
-    <input type="hidden" name="country" id="countryId" value="CA"
-    />
-<select name="province" class="states order-alpha browser-default custom-select custom-select- mb-3"   id="stateId"
-onChange={this.handleChange} 
-value={this.state.province}>
-    <option value="">Select Province</option>
-</select>
-</div>
-
-&nbsp;&nbsp;
-&nbsp;&nbsp;
-<div class="col">
-<select Async name="city" class="cities order-alpha browser-default custom-select custom-select-md mb-3" id="cityId"
-onChange={this.handleChange} 
-value={this.state.city}>
-    <option value="">Select City</option>
-</select>
-</div>
-</div>
-
-
-            <div className="text-right py-4 mt-3">
-            {/* <Link to= {{pathname :"/Upload", data: this.state.serviceType }}> */}
-              <MDBBtn className="btn btn-pink"  disabled={!this.state.serviceType||Number(this.state.minPrice)>= Number(this.state.maxPrice)||!this.state.city||!this.state.maxPrice||!this.state.minPrice||!this.state.province} onClick={this.createService} >     
-       
-                Continue
-                <MDBIcon far icon="angle-double-right" className="ml-2 fas fa-angle-right" />
-        
-              </MDBBtn>
-              {/* </Link> */}
+            <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css" />
+    
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12 col-sm-9">
+            {/* User profile */}
+            <div className="panel panel-default">
+              <div className="panel-heading">
+                <h4 className="panel-title">User profile</h4>
+              </div>
+              <div className="panel-body">
+                <div className="profile__avatar">
+                  <img src="https://bootdey.com/img/Content/avatar/avatar5.png" alt="..." />
+                </div>
+                <div className="profile__header">
+                  <h4>Richard Roe <small>Administrator</small></h4>
+                  <p className="text-muted">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non nostrum odio cum repellat veniam eligendi rem cumque magnam autem delectus qui.
+                  </p>
+                  <p>
+                    <a href="#">bootdey.com</a>
+                  </p>
+                </div>
+              </div>
             </div>
-          </form>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
-    <div className="spfooter1"> 
-    <footer1.footer1/>
-     </div>
-    </div>
+            {/* User info */}
+            <div className="panel panel-default">
+              <div className="panel-heading">
+                <h4 className="panel-title">User info</h4>
+              </div>
+              <div className="panel-body">
+                <table className="table profile__table">
+                  <tbody>
+                    <tr>
+                      <th><strong>Location</strong></th>
+                      <td>United States</td>
+                    </tr>
+                    <tr>
+                      <th><strong>Company name</strong></th>
+                      <td>Simpleqode.com</td>
+                    </tr>
+                    <tr>
+                      <th><strong>Position</strong></th>
+                      <td>Front-end developer</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            {/* Community */}
+            <div className="panel panel-default">
+              <div className="panel-heading">
+                <h4 className="panel-title">Community</h4>
+              </div>
+              <div className="panel-body">
+                <table className="table profile__table">
+                  <tbody>
+                    <tr>
+                      <th><strong>Comments</strong></th>
+                      <td>58584</td>
+                    </tr>
+                    <tr>
+                      <th><strong>Member since</strong></th>
+                      <td>Jan 01, 2016</td>
+                    </tr>
+                    <tr>
+                      <th><strong>Last login</strong></th>
+                      <td>1 day ago</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            {/* Latest posts */}
+            <div className="panel panel-default">
+              <div className="panel-heading">
+                <h4 className="panel-title">Latest posts</h4>
+              </div>
+              <div className="panel-body">
+                <div className="profile__comments">
+                  <div className="profile-comments__item">
+                    <div className="profile-comments__controls">
+                      <a href="#"><i className="fa fa-share-square-o" /></a>
+                      <a href="#"><i className="fa fa-edit" /></a>
+                      <a href="#"><i className="fa fa-trash-o" /></a>
+                    </div>
+                    <div className="profile-comments__avatar">
+                      <img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="..." />
+                    </div>
+                    <div className="profile-comments__body">
+                      <h5 className="profile-comments__sender">
+                        Richard Roe <small>2 hours ago</small>
+                      </h5>
+                      <div className="profile-comments__content">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, corporis. Voluptatibus odio perspiciatis non quisquam provident, quasi eaque officia.
+                      </div>
+                    </div>
+                  </div>
+                  <div className="profile-comments__item">
+                    <div className="profile-comments__controls">
+                      <a href="#"><i className="fa fa-share-square-o" /></a>
+                      <a href="#"><i className="fa fa-edit" /></a>
+                      <a href="#"><i className="fa fa-trash-o" /></a>
+                    </div>
+                    <div className="profile-comments__avatar">
+                      <img src="https://bootdey.com/img/Content/avatar/avatar6.png" alt="..." />
+                    </div>
+                    <div className="profile-comments__body">
+                      <h5 className="profile-comments__sender">
+                        Richard Roe <small>5 hours ago</small>
+                      </h5>
+                      <div className="profile-comments__content">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero itaque dolor laboriosam dolores magnam mollitia, voluptatibus inventore accusamus illo.
+                      </div>
+                    </div>
+                  </div>
+                  <div className="profile-comments__item">
+                    <div className="profile-comments__controls">
+                      <a href="#"><i className="fa fa-share-square-o" /></a>
+                      <a href="#"><i className="fa fa-edit" /></a>
+                      <a href="#"><i className="fa fa-trash-o" /></a>
+                    </div>
+                    <div className="profile-comments__avatar">
+                      <img src="https://bootdey.com/img/Content/avatar/avatar2.png" alt="..." />
+                    </div>
+                    <div className="profile-comments__body">
+                      <h5 className="profile-comments__sender">
+                        Richard Roe <small>1 day ago</small>
+                      </h5>
+                      <div className="profile-comments__content">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore, esse, magni aliquam quisquam modi delectus veritatis est ut culpa minus repellendus.
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-xs-12 col-sm-3">
+            {/* Contact user */}
+            <p>
+              <a href="#" className="profile__contact-btn btn btn-lg btn-block btn-info" data-toggle="modal" data-target="#profile__contact-form">
+                Contact user
+              </a>
+            </p>
+            <hr className="profile__contact-hr" />
+            {/* Contact info */}
+            <div className="profile__contact-info">
+              <div className="profile__contact-info-item">
+                <div className="profile__contact-info-icon">
+                  <i className="fa fa-phone" />
+                </div>
+                <div className="profile__contact-info-body">
+                  <h5 className="profile__contact-info-heading">Work number</h5>
+                  (000)987-65-43
+                </div>
+              </div>
+              <div className="profile__contact-info-item">
+                <div className="profile__contact-info-icon">
+                  <i className="fa fa-phone" />
+                </div>
+                <div className="profile__contact-info-body">
+                  <h5 className="profile__contact-info-heading">Mobile number</h5>
+                  (000)987-65-43
+                </div>
+              </div>
+              <div className="profile__contact-info-item">
+                <div className="profile__contact-info-icon">
+                  <i className="fa fa-envelope-square" />
+                </div>
+                <div className="profile__contact-info-body">
+                  <h5 className="profile__contact-info-heading">E-mail address</h5>
+                  <a href="mailto:admin@domain.com">admin@domain.com</a>
+                </div>
+              </div>
+              <div className="profile__contact-info-item">
+                <div className="profile__contact-info-icon">
+                  <i className="fa fa-map-marker" />
+                </div>
+                <div className="profile__contact-info-body">
+                  <h5 className="profile__contact-info-heading">Work address</h5>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
   );
 };
-
-}
+  }
