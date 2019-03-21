56f36fa513739a39cec348031f173785cb225b40
ermsci342 <32082256+ermsci342@users.noreply.github.com>
2019-03-21 05:44:31 -0400

    cool girl moved up in air
    cool girl moved up in air

--- src/components/Step1.js
+++ src/components/Step1.js
@@ -207,9 +207,12 @@ export class stepone extends Component {
 
 <MDBCol>
   <i class="fas fa-location-arrow" ></i>
-  <label>Address</label>
-  </MDBCol>
+  <label>Address  
 
+
+    
+  </label>
+  </MDBCol>
 
 
   <PlacesAutocomplete
