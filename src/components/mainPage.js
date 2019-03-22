d810c0d403f42b899f6672965e7e719becac6be4
ermsci342 <32082256+ermsci342@users.noreply.github.com>
2019-03-22 03:37:15 -0400

    new pics all
    new pics all

--- src/components/mainPage.js
+++ src/components/mainPage.js
@@ -105,16 +105,15 @@ export class mainPage extends Component {
         <img className="slide" alt="wed1" src={images['wed1.png']}/>
         <img className="slide" alt="wed3" src={images['wed3.png']}/>
         <img className="slide" alt="wed4" src={images['wed4.png']}/> 
-        <img className="slide" alt="wed5" src={images['wed5.png']}/>
+        <img className="slide" alt="wed5" src={images['wed5.png']}/> */}
         <img className="slide" alt="h25" src={images['h25.jpg']}/>
         <img className="slide" alt="m31" src={images['m31.jpg']}/>
-        <img className="slide" alt="h1" src={images['h1.jpg']}/> */}
-        {/* <img className="slide" alt="saree" src={images['saree.jpg']}/> */}
-        {/* <img className="slide" alt="s4" src={images['s4.jpg']}/> */}
-        {/* <img className="slide" alt="saree3" src={images['saree3.png']}/> */}
+        <img className="slide" alt="h1" src={images['h1.jpg']}/>
+        <img className="slide" alt="saree" src={images['saree.jpg']}/>
+        <img className="slide" alt="s4" src={images['s4.jpg']}/>
+        <img className="slide" alt="saree3" src={images['saree3.png']}/>
         <img className="slide" alt="holi" src={images['holi.jpg']}/>
 
-         {/* <img className="slide" alt="m4" src={images['m4.jpg']}/> */}
       
       </Slider>
       <img className="logoCM" alt="logo" src={images1['logoCM.png']} />
